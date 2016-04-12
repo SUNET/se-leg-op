adc560f992a20e8b4204f87c2d728139fd686abd
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-04-12 14:00:45 +0200

    Add Userinfo.__contains__ to support 'in' operator.

--- src/se_leg_op/userinfo.py
+++ src/se_leg_op/userinfo.py
@@ -15,6 +15,9 @@ class Userinfo(object):
     def __getitem__(self, item):
         return self._db[item]
 
+    def __contains__(self, item):
+        return item in self._db
+
     def get_claims_for(self, user_id, requested_claims):
         # type: (str, Mapping[str, Optional[Mapping[str, Union[str, List[str]]]]) -> Dict[str, Union[str, List[str]]]
         """
