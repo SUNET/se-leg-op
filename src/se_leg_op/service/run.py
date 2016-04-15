4e10e8b6b3928d6bdb6ed72d138817c80037bb84
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-04-15 12:00:25 +0200

    Make running example actually run.

--- src/se_leg_op/service/run.py
+++ src/se_leg_op/service/run.py
@@ -1,7 +1,7 @@
-from .app import oidc_provider_init_app
+from se_leg_op.service.app import oidc_provider_init_app
 
 name = 'oidc_provider'
-app = oidc_provider_init_app(name, {})
+app = oidc_provider_init_app(name)
 
 if __name__ == '__main__':
     app.logger.info('Starting {} app...'.format(name))
